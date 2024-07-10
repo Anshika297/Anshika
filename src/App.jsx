29ca06a91447458311b3774acbab164706828d2c
Anshika Choudhary <anshikac524@gmail.com>
2024-07-10 19:50:09 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+// import React, { useState } from 'react'
+// import './App.css'
+
+// const App = () => {
+//   return
+
+//  let [count,SetCount]= useState(bhopal);
+//  function fun1(){
+//   SetCount(indore);
+//  }
+
+ 
+
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={fun1}>bhopal</button>
+//     </div >
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import React, { useState } from 'react'
+
+// export const App = () => {
+//   let [time,SetTime]=useState()
+
+
+//   setInterval(() => {
+//     let date=new Date().toLocaleDateString;
+//     SetTime
+//   })
+
+//   return (
+//     <div>
+//       <p>{Time}</p>
+//     </div>
+//   )
+// }
+// export default App
+import React from 'react'
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
